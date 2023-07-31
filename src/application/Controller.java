aa0790738f944b0f727d2153ec19aa4f0d7ae467
Nathan Wardy <natewardy@gmail.com>
2023-07-31 12:27:31 -0400

    dashboard frame and Login hash and Exception Update

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -6,9 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Controller {
     @FXML
@@ -24,20 +28,22 @@ public class Controller {
     }
 
     public void Login(ActionEvent e) {
-        // Define the preset strings that are required to open the new dashboard window
-        String requiredUserID = "nate";
-        String requiredPassword = "wardy";
-
+        // Define the map of acceptable usernames and passwords
+        Map<String, String> acceptableUsers = new HashMap<>();
+        acceptableUsers.put("nate", "wardy");
+        acceptableUsers.put("Password", "User");
+        acceptableUsers.put("noah", "wardy");
+        
         // Retrieve the input from the text fields
         String enteredUserID = userIDField.getText();
         String enteredPassword = passwordField.getText();
 
         // Check if the entered UserID and Password match the preset strings
-        if (requiredUserID.equals(enteredUserID) && requiredPassword.equals(enteredPassword)) {
+        if (acceptableUsers.containsKey(enteredUserID) && acceptableUsers.get(enteredUserID).equals(enteredPassword)) {
             try {
                 // Load the FXML file for the dashboard window
                 Parent dashboardRoot = FXMLLoader.load(getClass().getResource("/hub.fxml"));
-                Scene dashboardScene = new Scene(dashboardRoot, 600, 400);
+                Scene dashboardScene = new Scene(dashboardRoot, 390, 330);
 
                 // Create a new stage for the dashboard window
                 Stage dashboardStage = new Stage();
@@ -46,21 +52,27 @@ public class Controller {
 
                 // Show the dashboard window
                 dashboardStage.show();
-                dashboardStage.setWidth(800);
-                dashboardStage.setHeight(600);
+                dashboardStage.setWidth(850);
+                dashboardStage.setHeight(620);
                 dashboardStage.setResizable(false);
+             
                 // Close the login stage
                 loginStage.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         } else {
-            // Handle the case when the preset strings do not match the entered text
-            System.out.println("Invalid UserID or Password. Please try again.");
-            // You can display an error message in the existing FXML, show a popup, or handle the error in a different way.
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Invalid Credentials");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid Credentials"); 
+            alert.showAndWait();
+            
         }
     }
-}
+	
+
+	}
 
 
 
