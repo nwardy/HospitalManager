2ad2b2b05c6fbaea0957d1d124c35529f0b308d6
Nathan Wardy <natewardy@gmail.com>
2023-08-07 14:13:31 -0400

    styling update

--- src/application/HubController.java
+++ src/application/HubController.java
@@ -1,10 +1,9 @@
 package application;
 
-	import javafx.scene.Parent;
-	import javafx.scene.Scene;
-	import javafx.stage.Stage;
-	import java.io.IOException;
-
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
@@ -38,7 +37,9 @@ import javafx.fxml.FXMLLoader;
 	        System.out.println("Apps clicked"); 
 	        switchView("/Appointments.fxml");
 	    }
-	
+	  
+	        
+	    
 	    private void switchView(String fxmlPath) throws IOException {
 	        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
 	        loader.setController(this); // set this instance as the controller
@@ -48,5 +49,9 @@ import javafx.fxml.FXMLLoader;
 	        hubStage.show();
 	    }
 	    
-	     
+	    @FXML
+	    public void logout() throws IOException {
+	    	System.out.println("logged out"); 
+	        hubStage.close();
 	    }
+	}
